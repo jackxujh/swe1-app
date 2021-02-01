07644bb0f20c4cf9d56012d78ba5b3048bca6b4b
Jack XU <jackxujh@msn.com>
2021-02-01 06:55:43 -0500

    Add deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+TARGET=$1
+
+PRODUCTION_TARGET="production"
+STATING_TARGET="staging"
+
+PRODUCTION_APP_HEROKU="swe-app-neo"
+STATING_APP_HEROKU="swe-app-neo-staging"
+
+if [ "$TARGET" = "$PRODUCTION_TARGET" ]
+then
+    DEPLOYMENT_APP=$PRODUCTION_APP_HEROKU
+elif [ "$TARGET" = "$STATING_TARGET" ]
+then
+    DEPLOYMENT_APP=$STATING_APP_HEROKU
+fi
+
+echo "Will deploy to $DEPLOYMENT_APP on Heroku."
+
+heroku auth:token
+heroku container:login
+
+heroku container:push web -a $DEPLOYMENT_APP
+heroku container:release web -a $DEPLOYMENT_APP
